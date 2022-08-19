08b7fda9f85c42b40c19784e842987c448b70c9f
Muhammad Asad Alvi <m.asad@gltexpress.com>
2022-08-19 20:34:30 +0500

    event binding with methods

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmName: ''
     };
   },
   methods: {
-    addCounter() {
-      this.counter++;
+    addCounter(num) {
+      this.counter = this.counter + num;
     },
-    subtractCounter() {
-      this.counter--;
+    subtractCounter(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    confirmInput() {
+      console.log(this.name);
+      this.confirmName = this.name;
+    },
+    submitForm() {
+      console.log("submitted");
+      alert('Submited!');
+    },
+    resetInput() {
+      this.name = "";
     }
   }
 });
